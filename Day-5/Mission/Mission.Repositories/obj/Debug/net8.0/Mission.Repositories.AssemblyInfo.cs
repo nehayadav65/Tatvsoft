efc261e84b70ebba2f4a63404acb20e26e94c016
Neha Yadav <ny656139@gmail.com>
2025-07-11 01:22:56 +0530

    Day-6

--- Day-5/Mission/Mission.Repositories/obj/Debug/net8.0/Mission.Repositories.AssemblyInfo.cs
+++ Day-5/Mission/Mission.Repositories/obj/Debug/net8.0/Mission.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6745dd9203bf8c5e8643c2ac76117e525cd799c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
